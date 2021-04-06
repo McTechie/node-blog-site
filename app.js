add5976f8ad718daa90ba8ed2d35dc52f5389864
Mcvean Soans <mc.soans02@gmail.com>
2021-04-06 20:07:13 +0530

    fix: minor change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://mctechie-admin:test@123456@node-blog-site.0udx0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbURI = "mongodb+srv://<Username>:<Password>@node-blog-site.0udx0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
